54f4e583449686f1df35a79125e8f9e9c8bc3e63
Gityuhi <yuhiprivate@gmail.com>
2025-05-18 17:37:25 +0900

    ゴルフスコア判定1回目のコミット

---
+++ ruby/golf_score.rb
@@ -0,0 +1,25 @@
+pars = gets.chomp.split(",").map(&:to_i)
+strokes = gets.chomp.split(",").map(&:to_i)
+
+pairs = pars.zip(strokes).map do |par, stroke| 
+  diff = par - stroke
+  if par == 5 && stroke == 1
+    "コンドル"
+  elsif par == 5 && stroke == 2
+    "アルバトロス"
+  elsif stroke == 1
+    "ホールインワン"
+  elsif  diff < -1 then "#{diff.abs}ボギー"
+  else
+    case diff
+      when 0 then "パー"
+      when 1 then "バーディ"
+      when 2 then "イーグル"
+      when 3 then "アルバトロス"
+      when 4 then "コンドル"
+      when -1 then "ボギー"
+    end
+  end
+end
+
+puts pairs.join(",")
